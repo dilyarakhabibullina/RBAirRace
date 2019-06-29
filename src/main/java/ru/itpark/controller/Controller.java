ea8cb4e8598a1f61a012bc9be9692229038fdde7
dilyarakhabibullina <khdilyara@rambler.ru>
2019-06-29 22:38:04 +0300

    все работает, добавлены детали по каждому пилоту

--- src/main/java/ru/itpark/controller/Controller.java
+++ src/main/java/ru/itpark/controller/Controller.java
@@ -3,6 +3,7 @@ package ru.itpark.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.itpark.domain.Pilot;
 import ru.itpark.service.PilotsService;
@@ -19,5 +20,16 @@ private final PilotsService service;
     model.addAttribute("pilotes", service.findAll());
     return "pilotes";
 }
+
+    @GetMapping("/details") // http://localhost:8080/add
+    public String addPage() {
+        return "details";
+    }
+
+    @GetMapping("/{id}")
+    public String details(@PathVariable int id, Model model) {
+        model.addAttribute("pilot", service.findById(id));
+        return "details";
+    }
 
 }
